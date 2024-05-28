bcd09f4a89fbace3bcac0867fe6ea61225ef2bdb
root <root@WIN-VQRNG6QVD9Q>
2024-05-29 02:01:08 +0300

    first commit

--- Мой вариант C/src/cat/cat.h
+++ Мой вариант C/src/cat/cat.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+#define MAX_LINE_LENGTH 4096 // Определение максимальной длины строки
+
+
 void process_file(FILE *file, int flag_b, int flag_e, int flag_n, int flag_s, int flag_t);
 
 #endif 
