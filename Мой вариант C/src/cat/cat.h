45216270d53ce8f4264b71df1896baf3c2ae3304
z4ng1ew <z4ng1ew@gmail.com>
2024-05-28 23:26:40 +0300

    first commit

---
+++ Мой вариант C/src/cat/cat.h
@@ -0,0 +1,10 @@
+#ifndef CAT_H
+#define CAT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void process_file(FILE *file, int flag_b, int flag_e, int flag_n, int flag_s, int flag_t);
+
+#endif 
