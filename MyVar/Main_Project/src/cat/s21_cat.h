1510d1430d787c8710bc3724d819e9907bcbfae5
root <root@WIN-VQRNG6QVD9Q>
2024-05-31 10:56:01 +0300

    second commit

---
+++ MyVar/Main_Project/src/cat/s21_cat.h
@@ -0,0 +1,22 @@
+#ifndef s21_cat
+#define s21_cat
+
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct flags;
+
+int get_opt(int argc, char **argv, struct flags *flag);
+
+void cat(char *path, struct flags);
+
+int s(struct flags flag, int *prev, int *ch, int *str);
+void b(struct flags flag, int *prev, int *ch, int *current_line);
+void n(struct flags flag, int *prev, int *current_line);
+void E(struct flags flag, int *prev, int *ch);
+void T(struct flags flag, int *ch);
+void v(struct flags flag, int *ch);
+
+#endif
--- MyVar/src/grap/my.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-int main(){
-
-    "hello";
-
-    return 0;
-}
